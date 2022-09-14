ec88ea7dbf5f2d88a0940ca5fc2935558f8a77bc
Anastasiya <slaastenkaa@gmail.com>
2022-09-14 21:51:37 +0400

    Добавление функционала

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 let navClose = document.querySelector('.page-header__closed');
 let navButton = document.querySelector('.navigation__button');
+let modalButton = document.querySelector('.modal-button');
+let modal = document.querySelector('.modal');
+let modalClose = document.querySelector('.modal-close');
 
 navClose.classList.remove('page-header__nojs');
 
@@ -10,5 +13,19 @@ navButton.addEventListener('click', function () {
   } else {
     navClose.classList.add('page-header__opened');
     navClose.classList.remove('page-header__closed');
+  }
+});
+
+modalButton.addEventListener('click', function () {
+  if (modalClose.classList.contains('modal-close')) {
+    modalClose.classList.remove('modal-close');
+    modalClose.classList.add('modal');
+  } else {
+    modal.addEventListener('click', function () {
+      if (modalClose.classList.contains('modal')) {
+        modalClose.classList.add('modal-close'); 
+        modalClose.classList.remove('modal'); 
+      }
+    });
   }
 });
